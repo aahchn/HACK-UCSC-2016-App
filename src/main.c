26ccb135d3b68eb6c7bee6b10d295ebe90009ed0
Warren Ngoun <steamb0t@users.noreply.github.com>
2016-01-30 17:48:55 -0800

    Add stats and settings window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
 
-static Window *s_main_window, *s_progress_window;
-static TextLayer *s_output_layer, *s_progresss_layer;
+static Window *s_main_window, *s_progress_window, *s_statistics_window, *s_settings_window;
+static TextLayer *s_output_layer, *s_progresss_layer, *s_statistics_layer, *s_settings_layer;
 static ActionBarLayer *action_bar;
 static GBitmap *menu_daily_up, *menu_stats_select, *menu_settings_down;
 static const bool animated = true;
@@ -13,10 +13,12 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_output_layer, "Statistics");
+  window_stack_push(s_statistics_window, animated);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_output_layer, "Settings");
+  window_stack_push(s_settings_window, animated);
 }
 
 static void click_config_provider(void *context) {
@@ -51,7 +53,7 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_output_layer);
 }
 
-static void menu_window_load(Window *window) {
+static void menu_progress_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
   window_set_background_color(s_progress_window, GColorCyan);
@@ -59,14 +61,50 @@ static void menu_window_load(Window *window) {
   s_progresss_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
   text_layer_set_font(s_progresss_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_background_color(s_progresss_layer, GColorClear);
-  text_layer_set_text(s_progresss_layer, "TEST a Button");
+  text_layer_set_text(s_progresss_layer, "THIS IS THE PROGRESS");
   text_layer_set_text_alignment(s_progresss_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
   
   layer_add_child(window_layer, text_layer_get_layer(s_progresss_layer));
 }
 
-static void menu_window_unload(Window *window) {
+static void menu_progress_window_unload(Window *window) {
   text_layer_destroy(s_progresss_layer);
+}
+
+static void menu_statistics_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+  window_set_background_color(s_statistics_window, GColorPurple);
+
+  s_statistics_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
+  text_layer_set_font(s_statistics_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_background_color(s_statistics_layer, GColorClear);
+  text_layer_set_text(s_statistics_layer, "THIS IS THE STATS");
+  text_layer_set_text_alignment(s_statistics_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
+  
+  layer_add_child(window_layer, text_layer_get_layer(s_statistics_layer));
+}
+
+static void menu_statistics_window_unload(Window *window) {
+  text_layer_destroy(s_statistics_layer);
+}
+
+static void menu_settings_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+  window_set_background_color(s_settings_window, GColorRed);
+
+  s_settings_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
+  text_layer_set_font(s_settings_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_background_color(s_settings_layer, GColorClear);
+  text_layer_set_text(s_settings_layer, "THIS IS THE SETTINGS");
+  text_layer_set_text_alignment(s_settings_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
+  
+  layer_add_child(window_layer, text_layer_get_layer(s_settings_layer));
+}
+
+static void menu_settings_window_unload(Window *window) {
+  text_layer_destroy(s_settings_layer);
 }
 
 static void init() {
@@ -85,8 +123,20 @@ static void init() {
   
   s_progress_window = window_create();
   window_set_window_handlers(s_progress_window, (WindowHandlers) {
-    .load = menu_window_load,
-    .unload = menu_window_unload,
+    .load = menu_progress_window_load,
+    .unload = menu_progress_window_unload,
+  });
+  
+  s_statistics_window = window_create();
+  window_set_window_handlers(s_statistics_window, (WindowHandlers) {
+    .load = menu_statistics_window_load,
+    .unload = menu_statistics_window_unload,
+  });
+  
+  s_settings_window = window_create();
+  window_set_window_handlers(s_settings_window, (WindowHandlers) {
+    .load = menu_settings_window_load,
+    .unload = menu_settings_window_unload,
   });
 }
 
